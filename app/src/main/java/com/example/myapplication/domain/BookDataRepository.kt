ffb8f669d8873ffa51cc537348aa3d61f938587d
Alireza1044 <alireza_1044@yahoo.com>
2021-05-15 18:01:19 +0430

    Implement book data repository

--- app/src/main/java/com/example/myapplication/domain/BookDataRepository.kt
+++ app/src/main/java/com/example/myapplication/domain/BookDataRepository.kt
@@ -1,4 +1,14 @@
 package com.example.myapplication.domain
 
+import com.example.myapplication.BookEntity
+import com.example.myapplication.data.BookDataSource
+
 class BookDataRepository {
+    fun getAllBooks(): Array<BookEntity> {
+        return BookDataSource.getAllBooks()
+    }
+
+    fun getBook(id: Int): BookEntity {
+        return BookDataSource.getBook(id)
+    }
 }
