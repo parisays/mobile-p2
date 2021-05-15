76361202fedcc3c01131f8150d19124577602474
Alireza1044 <alireza_1044@yahoo.com>
2021-05-15 17:57:57 +0430

    Implement detail viewmodel

--- app/src/main/java/com/example/myapplication/DetailViewModel.kt
+++ app/src/main/java/com/example/myapplication/DetailViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.myapplication
 
+import com.example.myapplication.domain.BookDataRepository
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val book = MutableLiveData<BookEntity>()
+
+    fun getBook(id:Int){
+        book.value = BookDataRepository.getBook(id)
+    }
 }
