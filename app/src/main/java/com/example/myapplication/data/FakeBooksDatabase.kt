200d0491d3cf9712422a0d0183ee0d3ae24540e2
Alireza1044 <alireza_1044@yahoo.com>
2021-05-15 17:48:26 +0430

    Add fake books to database

--- app/src/main/java/com/example/myapplication/data/FakeBooksDatabase.kt
+++ app/src/main/java/com/example/myapplication/data/FakeBooksDatabase.kt
@@ -8,38 +8,47 @@ object FakeBooksDatabase {
         name = "Oliver Twist",
         writer = "Charles Dickens",
         image = "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/chameleon/title-movie/252681_oliver_twist_2005_1400x2100_2.jpg",
-        genres="Novel",
+        genres = "Novel",
         page_count = 506
     )
 
     val book2 = BookEntity(
         id = 2,
-        name = "Oliver Twist",
-        writer = "Charles Dickens",
-        image = "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/chameleon/title-movie/252681_oliver_twist_2005_1400x2100_2.jpg",
-        genres="Novel",
-        page_count = 506
+        name = "Kafka on th Shore",
+        writer = "Haruki Murakami",
+        image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHFU_j93PPsbQGqoaZJnHH6-Emk_sIxG823SkoRTL0nvdEP41f",
+        genres = "Novel",
+        page_count = 467
     )
 
     val book3 = BookEntity(
         id = 3,
-        name = "Oliver Twist",
-        writer = "Charles Dickens",
+        name = "Dune",
+        writer = "Frank Herbert",
         image = "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/chameleon/title-movie/252681_oliver_twist_2005_1400x2100_2.jpg",
-        genres="Novel",
-        page_count = 506
+        genres = "Science Fiction",
+        page_count = 500
     )
 
     val book4 = BookEntity(
         id = 4,
-        name = "Oliver Twist",
-        writer = "Charles Dickens",
-        image = "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/chameleon/title-movie/252681_oliver_twist_2005_1400x2100_2.jpg",
-        genres="Novel",
-        page_count = 506
+        name = "The Call of Cthulhu",
+        writer = "H. P. Lovecraft",
+        image = "https://images-na.ssl-images-amazon.com/images/I/81JSqv9QqAL.jpg",
+        genres = "Horror",
+        page_count = 420
     )
 
-    fun getAllBooks(): Array<BookEntity>{
-        return arrayOf(book1,book2,book3,book4)
+    val book5 = BookEntity(
+        id = 5,
+        name = "When Nietzsche Wept",
+        writer = "Irvin D. Yalom",
+        image = "https://blackwells.co.uk/jacket/l/9780062009302.jpg",
+        genres = "Novel",
+        page_count = 352
+    )
+
+    fun getAllBooks(): Array<BookEntity> {
+        return arrayOf(book1, book2, book3, book4, book5)
     }
 }
