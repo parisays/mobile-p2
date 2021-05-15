9fae2a4f87a2b68afdc35c88890e9601fee3199b
Alireza1044 <alireza_1044@yahoo.com>
2021-05-15 20:30:59 +0430

    Refactor

--- app/src/main/java/com/example/myapplication/BooksListAdapter.kt
+++ app/src/main/java/com/example/myapplication/BooksListAdapter.kt
@@ -3,19 +3,15 @@ package com.example.mobilenetassignment2.ui.home
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 import com.example.myapplication.BookEntity
 import com.example.myapplication.OnItemClickListener
-import com.squareup.picasso.Picasso
 
-class HomeListAdapter(private val onItemClickListener: OnItemClickListener) :
-    RecyclerView.Adapter<HomeListAdapter.BookViewHolder>() {
+class BooksListAdapter(private val onItemClickListener: OnItemClickListener) :
+    RecyclerView.Adapter<BooksListAdapter.BookViewHolder>() {
 
-    var data = listOf<BookEntity>()
+    var data = arrayOf<BookEntity>()
 
     override fun getItemCount(): Int = data.size
 
@@ -30,12 +26,12 @@ class HomeListAdapter(private val onItemClickListener: OnItemClickListener) :
         holder.bind(data[position], onItemClickListener)
     }
 
-    fun reload(books: List<BookEntity>) {
+    fun reload(books: Array<BookEntity>) {
         data = books
         notifyDataSetChanged()
     }
 
-    class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class BookViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         fun bind(bookEntity: BookEntity, onItemClickListener: OnItemClickListener) {
             // TODO: update view
