a2e7747a8cae4b7aa421de37fb608ccd3eee4c67
pariays <parisa.yalsavar@gmail.com>
2021-05-16 20:37:30 +0430

    Complete BooksListAdapter

--- app/src/main/java/com/example/myapplication/BooksListAdapter.kt
+++ app/src/main/java/com/example/myapplication/BooksListAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication.home
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 import com.example.myapplication.BookEntity
 import com.example.myapplication.OnItemClickListener
+import com.squareup.picasso.Picasso
 
 class BooksListAdapter(private val onItemClickListener: OnItemClickListener) :
     RecyclerView.Adapter<BooksListAdapter.BookViewHolder>() {
@@ -33,11 +38,23 @@ class BooksListAdapter(private val onItemClickListener: OnItemClickListener) :
 
     class BookViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+        private val name = itemView.findViewById<TextView>(R.id.book_item_name)
+        private val image = itemView.findViewById<ImageView>(R.id.book_item_image)
+        private val writer = itemView.findViewById<TextView>(R.id.book_item_writer)
+
         fun bind(bookEntity: BookEntity, onItemClickListener: OnItemClickListener) {
-            // TODO: update view
+            try {
+                Picasso.get().load(bookEntity.image?.toUri()).into(image)
+//                Log.d(TAG, "Image loaded")
+            } catch (e: Exception) {
+//                Log.d(TAG, "Error loading artist image: $e")
+            }
+            name.text = bookEntity.name
+            writer.text = bookEntity.writer
             itemView.setOnClickListener {
                 onItemClickListener.onItemClicked(bookEntity)
             }
         }
+
     }
 }
